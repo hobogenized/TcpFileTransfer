398e553fee49bdd6cf53cb7f07f1b14e3d8eb0fd
Daniel W. Zhang <daniel.danielnew99@gmail.com>
2017-02-19 15:24:23 -0500

    Initial commit; need to refactor to use config instead of hardcoded values.

---
+++ MainServer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DatadventuresServer
+{
+	class MainServer
+	{
+		private TcpListener listener;
+		private const int PORT_NUMBER = 8888;
+		List<Byte[]> bytes;
+		public bool createThread;
+
+		public MainServer(int port)
+		{
+			IPEndPoint e = new IPEndPoint(IPAddress.Any, port);
+			listener = new TcpListener(e);
+			bytes = new List<Byte[]>();
+			createThread = true;
+		}
+
+		public async void handleNewConnection(int connectionNumber, List<Byte[]> bytes)
+		{
+			createThread = false;
+			var task = listener.AcceptTcpClientAsync();
+			TcpClient t = await task;
+			ThreadServer s = new ThreadServer(t, connectionNumber, bytes);
+			createThread = true;
+		}
+
+		static void Main(string[] args)
+		{
+			MainServer m = new MainServer(PORT_NUMBER);
+			m.listener.Start();
+			int connectionNumber = 0;
+			using (FileStream fs = File.Open("data.tar", FileMode.Open))
+			{
+				long len = fs.Length;
+				long count = 0;
+				byte[] bytes = new byte[1024 * 8];
+				while (count < len && (count = fs.Read(bytes, 0, bytes.Length)) > 0)
+				{
+					m.bytes.Add(bytes);
+					bytes = new byte[1024 * 8];
+				}
+			}
+			while (true)
+			{
+				if (m.createThread)
+				{
+					Console.WriteLine("Waiting for new connection");
+					m.handleNewConnection(connectionNumber, m.bytes);
+					connectionNumber++;
+				}
+			}
+		}
+	}
+}
---
+++ ThreadServer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace DatadventuresServer
+{
+    class ThreadServer
+    {
+        TcpClient localClient;
+        Thread t;
+        int nthClient;
+        List<Byte[]> bytes;
+
+        public ThreadServer(TcpClient tcp, int connectionNumber, List<Byte[]> bytesCopy)
+        {
+            localClient = tcp;
+            nthClient = connectionNumber;
+            t = new Thread(handleData);
+            t.Start();
+            bytes = bytesCopy;
+        }
+
+        public void handleData()
+        {
+            Console.WriteLine("Connection #" +
+                nthClient +
+                "established. Client is at " + 
+                localClient.Client.LocalEndPoint);
+
+            var stream = localClient.GetStream();
+            StreamReader s = new StreamReader(stream);
+            while (true)
+            {
+                string str = s.ReadLine();
+                if (str != null && str.Equals("DownloadGame"))
+                {
+                    using (StreamWriter w = new StreamWriter(stream)) {
+                        foreach (Byte[] b in bytes)
+                        {
+                            stream.Write(b, 0, b.Length);
+                        }
+                    }
+                    break;
+                }
+            }
+            Console.WriteLine("Finished transferring.");
+        }
+    }
+}
